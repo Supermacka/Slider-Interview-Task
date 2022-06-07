7eee40f004624db1d7ab14fc2361a4a92d06f73b
Colin Farkas <colin.farkas@iths.se>
2022-06-07 13:08:18 +0200

    Styling setup

--- slider-app/src/components/LoanCalculator/LoanCalculator.tsx
+++ slider-app/src/components/LoanCalculator/LoanCalculator.tsx
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import Sliders from "components/Sliders/Sliders";
 import MonthlyCostContext from "contexts/MonthlyCostContext";
+import styles from "components/LoanCalculator/loanCalculator.module.scss";
 
 const LoanCalculator: React.FC = () => {
   const { item } = useContext(MonthlyCostContext);
 
   return (
-    <div>
-      <header>
+    <div className={styles.calculator__container}>
+      <header className={styles.calculator__header}>
         <h1>Lånekalkyl</h1>
       </header>
-      <div>
+      <div className={styles.calculator__result}>
         <h4>Exempel på månadskostnad</h4>
         <h3>{item.monthlyCost} SEK / mån</h3>
       </div>
--- slider-app/src/components/Sliders/Sliders.tsx
+++ slider-app/src/components/Sliders/Sliders.tsx
@@ -1,7 +1,8 @@
 import React, { FormEvent, ChangeEvent, useEffect, useContext } from "react";
-import useForm from "components/utilities/useForm";
-import { calculateMonthlyCost } from "components/utilities/monthlyCostCalculation";
+import useForm from "utilities/useForm";
+import { calculateMonthlyCost } from "utilities/monthlyCostCalculation";
 import MonthlyCostContext from "contexts/MonthlyCostContext";
+import styles from "components/Sliders/sliders.module.scss";
 
 interface RangeProps {
   min: number;
@@ -55,37 +56,47 @@ const Sliders: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        <h4>Lånebelopp</h4>
-        <input
-          type="range"
-          name="loanAmount"
-          min={loanAmountPropValues.min}
-          max={loanAmountPropValues.max}
-          step={loanAmountPropValues.step}
-          value={loanAmount}
-          onChange={handleChange}
-        />
-        <output>{loanAmount} kr</output>
-        <output>{loanAmountPropValues.max} kr</output>
-      </label>
+      <div className={styles.sliders}>
+        <label className={styles.slider__container}>
+          <h4>Lånebelopp</h4>
+          <input
+            type="range"
+            name="loanAmount"
+            min={loanAmountPropValues.min}
+            max={loanAmountPropValues.max}
+            step={loanAmountPropValues.step}
+            value={loanAmount}
+            onChange={handleChange}
+            className={styles.slider__range}
+          />
+          <output>{loanAmount} kr</output>
+          <output className={styles.slider__max}>
+            {loanAmountPropValues.max} kr
+          </output>
+        </label>
 
-      <label>
-        <h4>Lånetid</h4>
-        <input
-          type="range"
-          name="loanDuration"
-          min={loanDurationtPropValues.min}
-          max={loanDurationtPropValues.max}
-          step={loanDurationtPropValues.step}
-          value={loanDuration}
-          onChange={handleChange}
-        />
-        <output>{loanDuration} år</output>
-        <output>{loanDurationtPropValues.max} år</output>
-      </label>
+        <label className={styles.slider__container}>
+          <h4>Lånetid</h4>
+          <input
+            type="range"
+            name="loanDuration"
+            min={loanDurationtPropValues.min}
+            max={loanDurationtPropValues.max}
+            step={loanDurationtPropValues.step}
+            value={loanDuration}
+            onChange={handleChange}
+            className={styles.slider__range}
+          />
+          <output>{loanDuration} år</output>
+          <output className={styles.slider__max}>
+            {loanDurationtPropValues.max} år
+          </output>
+        </label>
+      </div>
 
-      <button type="submit">Till ansökan</button>
+      <div className={styles.sliders__submit}>
+        <button type="submit">Till ansökan</button>
+      </div>
     </form>
   );
 };
